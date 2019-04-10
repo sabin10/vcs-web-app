b43142eaf067496793e707256229216c9795faca
sabin10 <hantusabin10@gmail.com>
2019-04-10 17:27:24 +0300

    Bind current LoggedIn user in /user view and displaying his name

--- src/main/java/com/sabinhantu/vcs/controller/AccountController.java
+++ src/main/java/com/sabinhantu/vcs/controller/AccountController.java
@@ -1,13 +1,31 @@
 package com.sabinhantu.vcs.controller;
 
+import com.sabinhantu.vcs.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AccountController {
 
+    @Autowired
+    private UserService userService;
+
+
     @GetMapping("/user")
-    public String getAccount() {
+    public String getAccount(Model model) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            String username = ((UserDetails)principal).getUsername();
+            model.addAttribute("currentuser", username);
+        } else {
+            String username = principal.toString();
+            model.addAttribute("currentuser", username);
+        }
+
         return "user";
     }
 }
--- src/main/java/com/sabinhantu/vcs/controller/IndexController.java
+++ src/main/java/com/sabinhantu/vcs/controller/IndexController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
@@ -13,7 +12,7 @@ public class IndexController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping({"", "/", "index"})
+    @GetMapping({"", "/", "index"})
     public String index(Model model) {
         model.addAttribute("users", userService.findAll());
         return "index";
