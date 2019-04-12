c9dfc5845710d7f981607ad76a994d2b18d0a28e
sabin10 <hantusabin10@gmail.com>
2019-04-12 15:44:37 +0300

    Repository Controller /{username}/{repositoryName}

---
+++ src/main/java/com/sabinhantu/vcs/controller/RepositoryController.java
@@ -0,0 +1,33 @@
+package com.sabinhantu.vcs.controller;
+
+import com.sabinhantu.vcs.model.Repository;
+import com.sabinhantu.vcs.model.User;
+import com.sabinhantu.vcs.repository.RepositoryRepository;
+import com.sabinhantu.vcs.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class RepositoryController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RepositoryRepository repositoryRepository;
+
+    @GetMapping("/{username}/{repositoryName}")
+    public String userRepository(@PathVariable final String username,
+                                 @PathVariable final String repositoryName,
+                                 Model model) {
+        User userRequested = userService.findByUsername(username);
+        Repository repositoryRequested = repositoryRepository.findByTitle(repositoryName);
+        if (userRequested == null || repositoryRequested == null) {
+            return "error";
+        }
+        model.addAttribute("repository", repositoryRequested);
+        return "repository";
+    }
+}
