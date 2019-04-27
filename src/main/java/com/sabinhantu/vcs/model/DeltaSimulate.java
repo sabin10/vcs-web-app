5bf21832f52b69788fc88c7f8649735536134a4d
sabin10 <hantusabin10@gmail.com>
2019-04-27 19:09:19 +0300

    DeltaSimulate entity which contains variables who will form new Chunks -> new Deltas -> new Patch from difflib library

---
+++ src/main/java/com/sabinhantu/vcs/model/DeltaSimulate.java
@@ -0,0 +1,103 @@
+package com.sabinhantu.vcs.model;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+public class DeltaSimulate{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    private Date createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "dbfile_id")
+    private DBFile file;
+
+    private String deltaType;
+
+    private int positionOriginal;
+    private String linesOriginal;
+
+    private int positionRevised;
+    private String linesRevised;
+
+    public DeltaSimulate() {
+    }
+
+    public DeltaSimulate(String deltaType, int positionOriginal, String linesOriginal, int positionRevised, String linesRevised) {
+        this.deltaType = deltaType;
+        this.positionOriginal = positionOriginal;
+        this.linesOriginal = linesOriginal;
+        this.positionRevised = positionRevised;
+        this.linesRevised = linesRevised;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public DBFile getFile() {
+        return file;
+    }
+
+    public void setFile(DBFile file) {
+        this.file = file;
+    }
+
+    public String getDeltaType() {
+        return deltaType;
+    }
+
+    public void setDeltaType(String deltaType) {
+        this.deltaType = deltaType;
+    }
+
+    public int getPositionOriginal() {
+        return positionOriginal;
+    }
+
+    public void setPositionOriginal(int positionOriginal) {
+        this.positionOriginal = positionOriginal;
+    }
+
+    public String getLinesOriginal() {
+        return linesOriginal;
+    }
+
+    public void setLinesOriginal(String linesOriginal) {
+        this.linesOriginal = linesOriginal;
+    }
+
+    public int getPositionRevised() {
+        return positionRevised;
+    }
+
+    public void setPositionRevised(int positionRevised) {
+        this.positionRevised = positionRevised;
+    }
+
+    public String getLinesRevised() {
+        return linesRevised;
+    }
+
+    public void setLinesRevised(String linesRevised) {
+        this.linesRevised = linesRevised;
+    }
+}
