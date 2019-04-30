f05f58c00964d2d3cca2fac39fd639dbd1363304
sabin10 <hantusabin10@gmail.com>
2019-05-01 01:05:04 +0300

    FileDisplay View and Controller

---
+++ src/main/java/com/sabinhantu/vcs/controller/FileDisplayController.java
@@ -0,0 +1,41 @@
+package com.sabinhantu.vcs.controller;
+
+import com.sabinhantu.vcs.form.FileForm;
+import com.sabinhantu.vcs.model.Branch;
+import com.sabinhantu.vcs.model.DBFile;
+import com.sabinhantu.vcs.repository.DBFileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Set;
+
+@Controller
+public class FileDisplayController {
+    @Autowired
+    CommitController commitController;
+
+    @Autowired
+    DBFileRepository dbFileRepository;
+
+    @GetMapping("/{username}/{projectUrl}/{branchName}/file/{fileName}")
+    public String displayFile(@PathVariable final String username,
+                              @PathVariable final String projectUrl,
+                              @PathVariable final String branchName,
+                              @PathVariable final String fileName,
+                              Model model) {
+
+        Branch currentBranch = commitController.getCurrentBranch(username, projectUrl, branchName);
+        Set<DBFile> files = currentBranch.getFiles();
+        for (DBFile file : files) {
+            if (file.getFileName().equals(fileName)) {
+                FileForm fileForm = new FileForm(file.getFileName(), new String(file.getData()));
+                model.addAttribute("fileForm", fileForm);
+            }
+        }
+        return "filedisplay";
+
+    }
+}
