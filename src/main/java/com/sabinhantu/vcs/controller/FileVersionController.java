d056a043a85bca702a6304718da46d0f06337cfa
sabin10 <hantusabin10@gmail.com>
2019-05-06 12:41:03 +0300

    file come back to previous versions all entities involved updated

--- src/main/java/com/sabinhantu/vcs/controller/FileVersionController.java
+++ src/main/java/com/sabinhantu/vcs/controller/FileVersionController.java
@@ -1,15 +1,17 @@
 package com.sabinhantu.vcs.controller;
 
+import com.sabinhantu.vcs.model.Branch;
 import com.sabinhantu.vcs.model.Commit;
 import com.sabinhantu.vcs.model.DBFile;
-import com.sabinhantu.vcs.repository.CommitRepository;
-import com.sabinhantu.vcs.repository.DBFileRepository;
-import com.sabinhantu.vcs.repository.ProjectRepository;
+import com.sabinhantu.vcs.model.DeltaSimulate;
+import com.sabinhantu.vcs.repository.*;
 import com.sabinhantu.vcs.service.DBFileStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Iterator;
 
 @Controller
 public class FileVersionController {
@@ -23,6 +25,9 @@ public class FileVersionController {
     private ProjectRepository projectRepository;
 
     @Autowired
+    private BranchRepository branchRepository;
+
+    @Autowired
     private BranchController branchController;
 
     @Autowired
@@ -31,6 +36,9 @@ public class FileVersionController {
     @Autowired
     private DBFileRepository dbFileRepository;
 
+    @Autowired
+    private DeltaSimulateRepository deltaSimulateRepository;
+
 
     @GetMapping("/{username}/{projectUrl}/{branchName}/{fileIdString}/{currCommitIdString}/getprevious")
     public String fileToPreviousVersion(@PathVariable final String username,
@@ -38,15 +46,8 @@ public class FileVersionController {
                                         @PathVariable final String branchName,
                                         @PathVariable final String fileIdString,
                                         @PathVariable final String currCommitIdString) {
-//        Project project = projectRepository.findByUrl(projectUrl);
-//        Commit currentCommit = commitRepository.getOne(currentCommitId);
-//        Branch branch = null;
-//        try {
-//            branch = branchController.getCurrentBranch(username, projectUrl, branchName);
-//        } catch (NullPointerException e) {
-//            e.printStackTrace();
-//            return "error";
-//        }
+        Branch branch = branchController.getCurrentBranch(username, projectUrl, branchName);
+
         Long currentCommitId = Long.parseLong(currCommitIdString);
         Commit currentCommit = commitRepository.getOne(currentCommitId);
 
@@ -54,20 +55,35 @@ public class FileVersionController {
         DBFile file = dbFileStorageService.getFile(fileId);
 
         int currentCommitIndex = file.getCommits().headSet(currentCommit).size();
-        int currIndex = 0;
-
 
         // avoid ConcurrentModificationException
         for (int i = 0; i < currentCommitIndex; i++) {
-            file.getCommits().remove(file.getLastCommit());
-        }
+            // remove deltasimulate from commit.deltaSimulateSet and from db
+            Commit lastCommit = file.getLastCommit();
+            Iterator<DeltaSimulate> itDelta = file.getLastCommit().getDeltaSimulateSet().iterator();
+            while (itDelta.hasNext()) {
+                DeltaSimulate deltaSimulate = itDelta.next();
+                if (deltaSimulate.getFile().getId().equals(fileId)) {
+                    itDelta.remove();
+                    deltaSimulateRepository.deleteById(deltaSimulate.getId());
+                }
+            }
+            commitRepository.save(lastCommit);
 
+            // remove commit from file.commits
+            file.getCommits().remove(file.getLastCommit());
+
+            // if lastCommit.deltaSimulateSet remains empty => delete from branch and from db
+            if (lastCommit.getDeltaSimulateSet().isEmpty()) {
+                branch.removeCommit(lastCommit);
+                branchRepository.save(branch);
+                commitRepository.deleteById(lastCommit.getId());
+            }
+        }
         String constructStringCurrCommit = commitController.
                 constructStringDataForCurrentCommit(file.getCommits(), currentCommit, file);
         file.setData(constructStringCurrCommit.getBytes());
         dbFileRepository.save(file);
-
-
         return "redirect:/" + username + "/" + projectUrl + "/" + branchName + "/file/" + file.getFileName();
     }
 }
--- src/main/java/com/sabinhantu/vcs/repository/DeltaSimulateRepository.java
+++ src/main/java/com/sabinhantu/vcs/repository/DeltaSimulateRepository.java
@@ -2,6 +2,12 @@ package com.sabinhantu.vcs.repository;
 
 import com.sabinhantu.vcs.model.DeltaSimulate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface DeltaSimulateRepository extends JpaRepository<DeltaSimulate, Long> {
+    @Modifying
+    @Transactional
+    void deleteById(Long id);
+
 }
